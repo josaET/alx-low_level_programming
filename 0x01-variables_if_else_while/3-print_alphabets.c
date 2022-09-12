1bdb281e8acc69f65ba636366f470ff70e0ffc0b
josa <josamerawi@gmail.com>
2022-09-12 15:59:56 -0700

    prints the alphabet in lowercase, and then in uppercase, followed by a new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main prints the alphabet in lowercase, and then in uppercase, followed by a new line 
  *
  * return: Always 0 (Success)
  */
