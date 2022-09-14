a5be00d67cb19dae4a66dc07f5fb3e5725b52f67
josa <josamerawi@gmail.com>
2022-09-14 02:13:20 -0700

    prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print _putchar using _putchar .
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i = "_putchar";
+
+	while (i)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
