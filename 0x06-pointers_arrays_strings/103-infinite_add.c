df38a087a8c84ca22d397550d9ae2af5bf0e2585
josa <josamerawi@gmail.com>
2022-09-23 06:39:49 -0700

    a function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,8 +5,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
  * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
+ * @n1: The string containing the first number to be added .
+ * @n2: The string containing the second number to be added .
  * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
  *
