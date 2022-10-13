240127ece5e437498912e876c59899cad7511bc0
josa <josamerawi@gmail.com>
2022-10-13 12:16:09 -0700

    a function that prints strings, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,6 +7,7 @@
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
