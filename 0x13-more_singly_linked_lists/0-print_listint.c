61700d023dd9631b0e2a90a53c083a018099c367
josaET <111006524+josaET@users.noreply.github.com>
2022-10-25 20:49:52 +0300

    prints all the elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,24 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+	size_t i = 0;
 
-	/* return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-
-		/*go to the next node*/
+		i++;
 		h = h->next;
-		/*count no of nodes*/
-		node_count++;
 	}
-
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (node_count);
+	return (i);
 }
